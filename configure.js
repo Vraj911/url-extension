b0655e41e21acdd3eefc6b159f7c4bc30c71fc45
Vraj911 <vraj9112005@gmail.com>
2025-07-10 02:08:46 +0530

    finish

--- configure.js
+++ configure.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 async function connectDB() {
-    return mongoose.connect(process.env.MONGO_URL, {
+    return mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
