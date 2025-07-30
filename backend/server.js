c26722b3cbb2272ed8ae2aa54e3aebeeed61dd20
Vraj911 <vraj9112005@gmail.com>
2025-07-30 12:48:45 +0530

    api-docs

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,31 @@ const connectDB = require('./configs/configure.js');
 const userRoute = require('./routes/userRoute.js');  
 const dotenv = require('dotenv');
 const cors = require('cors');
-const auth = require('./middleware/auth.js');       
+const auth = require('./middleware/auth.js');    
+const swaggerUi=require('swagger-ui-express');
+const swaggerJsdoc=require('swagger-jsdoc');   
 const authRoutes = require('./routes/authRoutes.js'); 
 dotenv.config();
 connectDB()
     .then(() => console.log('Database connected successfully'))
     .catch((err) => console.error('Database connection failed:', err));
+const swaggerOptions={
+ definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "URL Shortener API",
+            version: "1.0.0",
+            description: "API documentation for URL Shortener backend",
+        },
+        servers: [
+            { url: "http://localhost:5000" }, 
+            { url: "https://url-extension-1.onrender.com" } 
+        ],
+    },
+    apis: ["./routes/*.js"],
+};
+const swaggerSpec=swaggerJsdoc(swaggerOptions);
+app.use('/api-docs',swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(cors({
     origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     methods: ['GET', 'POST'],
